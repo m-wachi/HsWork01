75169944318bb8eaf2e4a5bde2da396547655ff2
Ubuntu <ubuntu@ip-172-31-31-154.us-west-2.compute.internal>
2018-01-20 23:46:53 +0900

    just commit

---
+++ work01/parsec01.hs
@@ -0,0 +1,9 @@
+import Text.ParserCombinators.Parsec
+
+func01 :: String -> Either ParseError Char
+func01 v1 = parse anyChar "" v1
+
+var1 = 10
+
+
+
---
+++ work01/test02.hs
@@ -0,0 +1,5 @@
+a = 10
+b="hey jude"
+c=[("key1", "val1"), ("key2", "val02")]
+
+
