6719022973e835f83a5da557954446beba3b7a1e
m-wachi <m-wachi-ipad@deb01.us-east1-b.c.daring-octane-215816.internal>
2019-01-04 00:21:05 +0900

    ins_t_date.hs code is almost complated.

--- pg01/ins_t_date.hs
+++ pg01/ins_t_date.hs
@@ -8,22 +8,34 @@ insRecord conn dayVal = do
   let dn = Cal.toModifiedJulianDay dayVal
   -- dow: 1:Mon, 2:Tue ... 7:Sun
   let (_, _, dow) = CalWD.toWeekDate dayVal
-  print dow
+  -- print dow
+  let tmp = (dn - 4 - (fromIntegral dow)) :: Integer
+  let wn = tmp `div` (7::Integer) 
+  -- print wn
   -- let downum = CalW.fromEnum dow
-  HDBC.run conn "insert into t_date values(?, ?, 0, ?)"
-    [HDBC.toSql dayVal, HDBC.toSql dn, HDBC.toSql dow]
+  HDBC.run conn "insert into t_date values(?, ?, ?, ?)"
+    [HDBC.toSql dayVal, HDBC.toSql dn, HDBC.toSql wn, HDBC.toSql dow]
 
+
+genDayList :: Cal.Day -> Cal.Day -> [Cal.Day]
+genDayList from to =
+  takeWhile (<= to) $ iterate succ from
 
 
 main = do
   conn <- PgHDBC.connectPostgreSQL "host='localhost' dbname='user01db' user='user01' password='user01'"
-  let a = Cal.fromGregorian 2019 1 1
+  let a = Cal.fromGregorian 2015 1 1
   print a
-  c <- insRecord conn a
-  print c
+  let b = Cal.fromGregorian 2015 1 10
+  print b
+  let c = genDayList a b
+  -- print c
+  let f1 = insRecord conn
+  d <- mapM (insRecord conn) c -- use mapM if function is Action(IO Monad)
+  -- print d
   HDBC.commit conn
-  res <- HDBC.quickQuery conn "select * from t_date" []
-  putStrLn $ show res
+  -- res <- HDBC.quickQuery conn "select * from t_date" []
+  -- putStrLn $ show res
   HDBC.disconnect conn
 
 
